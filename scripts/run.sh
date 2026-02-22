7ff662e5363476ef512d891f58f0c5f3ea1ced71
Hashyp <hashyp@gmail.com>
2026-02-22 16:18:05 +0100

    Use absolute main.go path for goex alias

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -eu
 
-docker exec goex sh -lc 'cd /workspaces/defaultdevcontainer && go run ./cmd/goex'
+docker exec goex sh -lc 'go run /workspaces/defaultdevcontainer/cmd/goex/main.go'
